71e49f775e1e6ad52ca05266ecd23f32e15202a2
PixerJason <109621806+PixerJason@users.noreply.github.com>
2023-01-26 00:29:12 +0300

    Add files via upload

---
+++ backup/testsend.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$(curl https://raw.githubusercontent.com/PixerJason/project/main/ipvps.txt | grep $MYIP | awk '{print $3}')
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/PixerJason"
+exit 0
+fi
+clear
+email=$(cat /home/email)
+date=$(date +"%Y-%m-%d");
+IP=$(wget -qO- ipinfo.io/ip);
+echo -e "
+i Don't even know what am doing
+IP VPS : $IP
+Date : $date
+" | mail -s "Your  Email" $email
---
+++ ssh/about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script By Pixer Jason      #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By Akbar Maulana                     #"
+echo -e "#                                               #"
+echo -e "================================================="
+echo -e "# Thanks To                                     #"
+echo -e "================================================="
+echo -e "# GhostCracker(Pixer Jason)                     #"
+echo -e "# Jason Group                                   #"
+echo -e "# https://t.me/Ghostcrack3r                     #"
+echo -e "# https://t.me/PixerJason                       #"
+echo -e "================================================="
